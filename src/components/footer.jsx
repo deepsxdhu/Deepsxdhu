d572c8a7bd0f1d1035d9b868ec1c07a3cf164501
Deepsxdhu <deepsxdhu@gmail.com>
2025-11-26 16:53:31 +0530

    Updated Footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <div className="pt-[100px]  relative pb-[50px] text-sm dark:text-stone-300 ">
-            <p>Design & Developed by Deepsxdhu</p> 
+            <p>Design & Developed by <b>Deepsxdhu</b></p> 
             <p> &copy; 2025. All rights reserved</p>
         </div>
     )
